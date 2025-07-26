a3652a973f9368c42d50aedee6609e6c0fa8ab3a
晨魔 <morningdemon515@outlook.com>
2025-07-26 21:53:11 +0800

    Great

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,35 @@
+
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+
+#include "LoadImage.h"
 
 int main()
 {
     GLFWwindow* window;
 
     glfwInit();
-
+    
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     window = glfwCreateWindow(800, 600, "MDPIV", NULL, NULL);
 
+    Image ico;
+    Load_Image_RGBA("resources/icon/MorningDemon.jpg", &ico);
+
+    GLFWimage icon;
+    icon.width = ico.width;
+    icon.height = ico.height;
+    icon.pixels = ico.pixels;
+    glfwSetWindowIcon(window, 1, &icon);
+
     glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
     while(!glfwWindowShouldClose(window))
     {
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
---
+++ src/Window.h
@@ -0,0 +1,17 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+class Window
+{
+private:
+    
+public:
+    Window();
+    Window(int Width, int Height);
+    ~Window();
+
+    void SetTitle();
+    
+};
+
+#endif
