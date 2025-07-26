a325c0d0fae5e5f0f1a46732ce75c92ac9a90a88
晨魔 <morningdemon515@outlook.com>
2025-07-26 16:00:51 +0800

    FXXX

--- src/Main.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
-
-static std::string Window_Title = "MDPIV";
-
-const char* ReadFile(const char* filename);
-float DeltaTime();
